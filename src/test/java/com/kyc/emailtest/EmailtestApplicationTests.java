98ce5662dbfcab0bb84b7311a252607bef3c5612
umcoder13 <68634885+umcoder13@users.noreply.github.com>
2022-11-17 15:04:08 +0900

    Redis 연결 관련 테스트코드 작성.

--- src/test/java/com/kyc/emailtest/EmailtestApplicationTests.java
+++ src/test/java/com/kyc/emailtest/EmailtestApplicationTests.java
@@ -1,13 +1,35 @@
 package com.kyc.emailtest;
 
+import com.kyc.emailtest.util.RedisUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class EmailtestApplicationTests {
 
+    @Autowired
+    private RedisUtil redisUtil;
+
+
     @Test
-    void contextLoads() {
+    public void redisTest () throws Exception {
+        //given
+        String email = "test@test.com";
+        String code = "aaa111";
+
+        //when
+        redisUtil.setDataExpire(email, code, 60 * 60L);
+
+        //then
+        Assertions.assertTrue(redisUtil.existData("test@test.com"));
+        Assertions.assertFalse(redisUtil.existData("test1@test.com"));
+        Assertions.assertEquals(redisUtil.getData(email), "aaa111");
+
     }
 
 }
